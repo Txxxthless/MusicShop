822a91e6009e189578651d7da2e1d6fd92ed8f0f
Leshchov Pavlo <114981628+Txxxthless@users.noreply.github.com>
2023-03-29 20:21:33 +0300

    Add files via upload

---
+++ MusicShopApplication/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using MusicShop.DAL;
+using MusicShop.DAL.Interfaces;
+using MusicShop.DAL.Repositories;
+using MusicShop.Domain.Entity;
+using MusicShop.Service.Interfaces;
+using MusicShop.Service.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+        options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+    });
+
+//db register
+builder.Services.AddDbContext<ApplicationDataBaseContext>(options => options.UseSqlServer(connectionString));
+//add user repo
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+//add user service
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
