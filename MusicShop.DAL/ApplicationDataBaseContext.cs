4ceafbb6fbe503f38c6e2441eab608962b63cdd2
Leshchov Pavlo <114981628+Txxxthless@users.noreply.github.com>
2023-03-29 20:23:34 +0300

    Add files via upload

---
+++ MusicShop.DAL/ApplicationDataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicShop.Domain.Entity;
+
+namespace MusicShop.DAL
+{
+    public class ApplicationDataBaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        public ApplicationDataBaseContext(DbContextOptions<ApplicationDataBaseContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ MusicShop.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MusicShop.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Create(T entity);
+        Task Update(T entity);
+        IQueryable<T> GetAll();
+        Task Delete(T entity);
+    }
+}
