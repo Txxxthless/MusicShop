5dbceb2af2f9fe7414b3ac020051d3372b8bf163
Leshchov Pavlo <114981628+Txxxthless@users.noreply.github.com>
2023-04-01 13:30:36 +0300

    Add files via upload

--- MusicShopApplication/Controllers/ProductsController.cs
+++ MusicShopApplication/Controllers/ProductsController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MusicShop.Domain.Entity;
-using MusicShop.Domain.ViewModels;
 using MusicShop.Service.Interfaces;
 
 namespace MusicShopApplication.Controllers
@@ -13,13 +12,28 @@ namespace MusicShopApplication.Controllers
             _productService = productService;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Product(int id)
+        {
+            var response = await _productService.GetProduct(id);
+            if (response.Status == MusicShop.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+            return RedirectToAction("Error");
+        }
+
+        [HttpGet]
         public async Task<IActionResult> Guitars()
         {
-            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(MusicShop.Domain.Enum.InstrumentType.String);
+            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(
+                MusicShop.Domain.Enum.InstrumentType.String);
             if (response.Status == MusicShop.Domain.Enum.StatusCode.OK)
             {
                 return View(response.Data);
@@ -27,9 +41,11 @@ namespace MusicShopApplication.Controllers
             return RedirectToAction("Index", "Products");
         }
 
+        [HttpGet]
         public async Task<IActionResult> Keyboards()
         {
-            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(MusicShop.Domain.Enum.InstrumentType.Keyboard);
+            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(
+                MusicShop.Domain.Enum.InstrumentType.Keyboard);
             if (response.Status == MusicShop.Domain.Enum.StatusCode.OK)
             {
                 return View(response.Data);
@@ -37,9 +53,11 @@ namespace MusicShopApplication.Controllers
             return RedirectToAction("Index", "Products");
         }
 
+        [HttpGet]
         public async Task<IActionResult> Percussion()
         {
-            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(MusicShop.Domain.Enum.InstrumentType.Percussion);
+            DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(
+                MusicShop.Domain.Enum.InstrumentType.Percussion);
             if (response.Status == MusicShop.Domain.Enum.StatusCode.OK)
             {
                 return View(response.Data);
@@ -47,6 +65,7 @@ namespace MusicShopApplication.Controllers
             return RedirectToAction("Index", "Products");
         }
 
+        [HttpGet]
         public async Task<IActionResult> Accessories()
         {
             DataBaseResponse<List<Product>> response = await _productService.GetProductsByType(MusicShop.Domain.Enum.InstrumentType.Accessory);
